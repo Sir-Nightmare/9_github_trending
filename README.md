9aa1e274fa873d899d3d9e3696234b47d2b9407e
Sir Nightmare <ivantochilkin@gmail.com>
2016-10-14 17:13:57 +0300

    the task was done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 9_github_trending
+
+This script shows 20 the most trending repositories created for the last 7 days.
+It also shows number of open issues for each repository.
+
+You can run the script using following command: _python github_trending.py_
+
+You will need **requests** package to run this script. 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,32 @@
+import requests
+from datetime import date, timedelta
+
+GITHUB_REP_API = 'https://api.github.com/search/repositories'
+NUMBER_OF_TOP_REPS = 20
+
+
+def data_week_ago():
+    return str(date.today() - timedelta(days=7))
 
 
 def get_trending_repositories(top_size):
-    pass
+    created_date = 'created:>=' + data_week_ago()
+    params_for_request = {'q': created_date, 'sort': 'stars', 'order': 'desc'}
+    repository_data = requests.get(GITHUB_REP_API, params=params_for_request).json()
+    top_trend_repositories = repository_data['items'][:top_size]
+    return top_trend_repositories
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def print_repo_list(repo_list):
+    print('Top trending repositories created last week:\n')
+    print('{0:40s} {1:5s}  {2:4s}     {3:30s}'.format('Repository name',
+                                                      'Stars', 'Open issues', 'Repository URL'))
+    print('-' * 100)
+    for repo in repo_list:
+        print('{0:40s} {1:5d}    {2:4d}       {3:30s}'.format(repo['name'],
+                                                              repo['stargazers_count'], repo['open_issues_count'],
+                                                              repo['html_url']))
+
 
 if __name__ == '__main__':
-    pass
+    print_repo_list(get_trending_repositories(NUMBER_OF_TOP_REPS))
